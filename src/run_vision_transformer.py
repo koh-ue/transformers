48f21c12a1ef3148a6a0622f2175eda786e52bd6
Koh Uehara <koh@naboo.local>
2023-06-27 02:36:29 +0900

    check train_set

--- src/run_vision_transformer.py
+++ src/run_vision_transformer.py
@@ -13,7 +13,7 @@ import torchvision.transforms as transforms
 
 sys.path.append(".")
 
-from src.vision_transformer.vit_model import *
+from src.vit_model import *
 
 if __name__ == "__main__":
     batch_size = 100
@@ -23,6 +23,8 @@ if __name__ == "__main__":
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
     train_set = torchvision.datasets.CIFAR10(root='../data', train=True, download=True, transform=transform)
+    print(train_set)
+    sys.exit()
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=2)
     test_set = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transform)
     test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=2)
